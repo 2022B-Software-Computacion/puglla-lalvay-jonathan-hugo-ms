2b29bd6c0fd6ae5eef350a11f1f70b8bbcffb5c2
Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>
2022-12-13 10:48:58 -0500

    Device Manager
    Virtual Device was configured.

--- 00-Kotlin/nuevo/src/main/kotlin/main.kt
+++ 00-Kotlin/nuevo/src/main/kotlin/main.kt
@@ -108,7 +108,55 @@ fun main() {
     // return@map valorActual + 15
     // }
     println(respuestaMapDos)
+
+    // Filter -> FILTRAR EL ARREGLO
+    // 1) Devolver una expresión (TRUE o FALSE)
+    // 2) Nuevo arreglo filtrado
+
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter {valorActual: Int ->
+            val mayoresACinco: Boolean = valorActual > 5 // Expresión Condición
+            return@filter mayoresACinco
+        }
+
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+
+    // OR AND
+    // OR -> ANY (Alguno cumple?)
+    // AND -> ALL (Todos cumplen?)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any{valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println("Respuesta Any: " + respuestaAny)
+
+    val respuestaAll: Boolean = arregloDinamico
+        .all{valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println("Respuesta All: " + respuestaAll) // false
+
+    // REDUCE -> Valor acumulado
+    // Valor acumulado = 0 (Siempre 0 en lenguaje Kotlin)
+    // [1, 2, 3, 4, 5] -> Sumeme todos los valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 + 1 = 1 -> Iteracion 1
+    // valorIteracion2 = valorIteracion1 + 2 = 1 + 2 = 3 -> Iteracion 2
+    // valorIteracion3 = valorIteracion2 + 3 = 3 + 3 = 6 -> Iteracion 3
+    // valorIteracion4 = valorIteracion3 + 4 = 6 + 4 = 10 -> Iteracion 4
+    // valorIteracion5 = valorIteracion4 + 5 = 10 + 5 = 15 -> Iteracion 5
+
+    println("Operador REDUCE: ")
+    val respuestaReduce: Int = arregloDinamico
+        .reduce{ // acumulado = 0 -> SIEMPRE EMPIEZA EN 0
+            acumulado: Int, valorActual: Int ->
+            return@reduce (acumulado + valorActual) // Lógica negocio
+        }
+    println("Respuesta reduce: " + respuestaReduce) //78
 }
+
 
 
 // Unit == void
