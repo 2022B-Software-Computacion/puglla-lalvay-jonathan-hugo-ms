9b6d9a013c80481c0a0670a7b590a85cffe06c0e
Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>
2022-12-20 10:54:37 -0500

    Deployment 1.0 (1)
    New deployment was sent over firebase.

--- 01-android/app/src/main/java/com/example/jhpl_application/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/jhpl_application/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.jhpl_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Botón del ciclo de vida
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
 
