2e02649cf3b9a34a39b4a8767c600981910b9573
Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>
2023-01-25 12:46:06 -0500

    Recycler View
    Recycler View implementation is done.

--- 01-android/app/src/main/java/com/example/jhpl_application/FRecyclerViewAdaptadorNombreCedula.java
+++
@@ -1,4 +0,0 @@
-package com.example.jhpl_application;
-
-public class FRecyclerViewAdaptadorNombreCedula {
-}
---
+++ 01-android/app/src/main/java/com/example/jhpl_application/FRecyclerViewAdaptadorNombreCedula.kt
@@ -0,0 +1,63 @@
+package com.example.jhpl_application
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FRecyclerViewAdaptadorNombreCedula(
+    private val contexto: GRecycleView,
+    private val lista: ArrayList<BEntrenador>,
+    private val recyclerView: RecyclerView
+): RecyclerView.Adapter<FRecyclerViewAdaptadorNombreCedula.MyViewHolder>() {
+    inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val nombreTextView: TextView
+        val cedulaTextView: TextView
+        val likesTextView: TextView
+        val accionButton: Button
+        var numeroLikes = 0
+        init {
+            nombreTextView = view.findViewById(R.id.tv_nombre)
+            cedulaTextView = view.findViewById(R.id.tv_cedula)
+            likesTextView = view.findViewById(R.id.tv_likes)
+            accionButton = view.findViewById<Button>(R.id.btn_dar_like)
+            accionButton.setOnClickListener { anadirLike() }
+        }
+        fun anadirLike(){
+            numeroLikes += 1
+            likesTextView.text = numeroLikes.toString()
+        }
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+        val itemView = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.recycler_view_vista,
+                parent,
+                false
+            )
+        return MyViewHolder(itemView)
+    }
+
+    // Setear los datos para la iteración
+    override fun onBindViewHolder(
+        holder: FRecyclerViewAdaptadorNombreCedula.MyViewHolder,
+        position: Int
+    ) {
+        val entrenadorActual = this.lista[position]
+        holder.nombreTextView.text = entrenadorActual.nombre
+        holder.cedulaTextView.text = entrenadorActual.descripcion
+        holder.accionButton.text = "Like ${entrenadorActual.nombre}"
+        holder.likesTextView.text = "0"
+    }
+
+    // Tamaño del arreglo
+    override fun getItemCount(): Int {
+        return this.lista.size
+    }
+}
