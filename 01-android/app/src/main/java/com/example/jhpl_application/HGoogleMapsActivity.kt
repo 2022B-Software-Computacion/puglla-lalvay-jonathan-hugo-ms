89b352898b1f1e4d45c9f22131bcd215f3e652c5
Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>
2023-02-02 11:00:58 -0500

    Quicentro Google Maps
    Google Maps API Implementation is done.

--- 01-android/app/src/main/java/com/example/jhpl_application/HGoogleMapsActivity.kt
+++ 01-android/app/src/main/java/com/example/jhpl_application/HGoogleMapsActivity.kt
@@ -3,14 +3,14 @@ package com.example.jhpl_application
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PackageManagerCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import java.util.zip.ZipEntry
 
 class HGoogleMapsActivity : AppCompatActivity() {
@@ -20,6 +20,8 @@ class HGoogleMapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hgoogle_maps2)
+        solicitarPermisos()
+        iniciarLogicaMapa()
     }
 
     fun iniciarLogicaMapa(){
@@ -29,6 +31,44 @@ class HGoogleMapsActivity : AppCompatActivity() {
             if (googleMap != null) {
                 mapa = googleMap
                 establecerConfiguracionMapa()
+                val zoom = 17f
+                val quicentro = LatLng(
+                    -0.17556708490271092, -78.48014901143776
+                )
+                val titulo = "Quicentro"
+                val markQUicentro = anadirMarcador(quicentro, titulo)
+                markQUicentro.tag = titulo
+
+                val poliLineaUno = googleMap.addPolyline(
+                    PolylineOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1759187040647396,
+                                -78.48506472421384),
+                            LatLng(-0.17632468492901104,
+                                -78.48265589308046),
+                            LatLng(-0.17746143130181483,
+                                -78.4770533307815)
+                        )
+                )
+
+                // POLIGONO
+                val poligonoUno = googleMap
+                    .addPolygon(
+                        PolygonOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1771546902239471,
+                                -78.48344981486125768),
+                                LatLng(-0.117968981486125768,
+                                -78.48269198043828),
+                                LatLng(-0.17710958124147777,
+                                -78.48142892291516),
+                            )
+                    )
+                poligonoUno.fillColor = -0xc771c4
+                poligonoUno.tag = "poligono-2" // ID
+                escucharListeners()
             }
 
         }
@@ -51,12 +91,37 @@ class HGoogleMapsActivity : AppCompatActivity() {
         }
     }
 
-    fun anadirMarcador(latLng: LatLng, title: String){
-        mapa.addMarker(
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("map", "setOnPolygonClickListener $it")
+            it.tag // ID
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("map", "setOnPolylineClickListener $it")
+            it.tag // ID
+        }
+        mapa.setOnMarkerClickListener {
+            Log.i("map","setOnMarkerClickListener $it")
+            it.tag // ID
+            return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraMoveStartedListener $it")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdleListener")
+        }
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String): Marker{
+        return mapa.addMarker(
             MarkerOptions()
                 .position(latLng)
                 .title(title)
-        )
+        )!!
     }
 
     fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f) {
