671affeec307ad58a9cf53cb9e5db2c8ccb166a7
Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>
2023-02-08 12:59:21 -0500

    Firebase Authentication
    Firebase Authentication has been implemente

---
+++ 01-android/app/src/main/java/com/example/jhpl_application/IFirebaseUIAuth.kt
@@ -0,0 +1,60 @@
+package com.example.jhpl_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.IdpResponse
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+
+class IFirebaseUIAuth : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res: FirebaseAuthUIAuthenticationResult ->
+        if (res.resultCode === RESULT_OK) {
+            if(res.idpResponse != null) {
+                seLogeo(res.idpResponse!!)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ifirebase_uiauth)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        btnLogin.setOnClickListener {
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            // Create and launch sign-in intent
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+            // RESPUESTA DEL INTENT DE LOGIN
+            signInLauncher.launch(signInIntent)
+        }
+    }
+
+    fun seLogeo(
+        res: IdpResponse
+    ) {
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.visibility = View.VISIBLE
+        btnLogin.visibility = View.INVISIBLE
+        if (res.isNewUser) {
+            registrarUsuarioPorPrimeraVez(res)
+        }
+    }
+
+    fun registrarUsuarioPorPrimeraVez(
+        usuario: IdpResponse
+    ) {
+        // Firestore
+    }
+}
--- 01-android/app/src/main/java/com/example/jhpl_application/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/jhpl_application/MainActivity.kt
@@ -108,6 +108,12 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener{
                 irActividad(HGoogleMapsActivity::class.java)
             }
+
+        val botonFirebaseUI = findViewById<Button>(R.id.btn_intent_firebase_ui)
+        botonFirebaseUI
+            .setOnClickListener {
+                irActividad(IFirebaseUIAuth::class.java)
+            }
     }
 
     fun abrirActividadConParametros(
